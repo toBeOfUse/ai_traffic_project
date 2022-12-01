d265c8603a515987654175f3ffe67207334ac530
toBeOfUse <mitchjacov@gmail.com>
2022-12-01 14:36:57 -0500

    put dataset loading inside function

--- create_datasets.py
+++ create_datasets.py
@@ -12,27 +12,26 @@ with open("category_vector.csv") as label_file:
 print("labels:", labels)
 print("first label vector:", vectors[0])
 
-img_size = 255
-
-print("loading images as dataset...")
-training, validation = tf.keras.utils.image_dataset_from_directory(
-    Path("./udacity-2/object-dataset/smaller/").absolute(),
-    labels=vectors,
-    label_mode="categorical",
-    color_mode="rgb",
-    image_size=(img_size, img_size),
-    validation_split=0.8,
-    seed=2**20,
-    subset="both",
-    interpolation="bilinear",
-    crop_to_aspect_ratio=False,
-)
-
-print(training)
-print(validation)
-
-def get_datasets(small_test: bool):
+def get_datasets(
+    img_size: int, small_test: bool=False
+) -> tuple[tf.data.Dataset, tf.data.Dataset]:
+    """
+    Returns two datasets in a tuple: (training, validation)
+    """
+    print("loading images as dataset...")
+    training, validation = tf.keras.utils.image_dataset_from_directory(
+        Path("./udacity-2/object-dataset/smaller/").absolute(),
+        labels=vectors,
+        label_mode="categorical",
+        color_mode="rgb",
+        image_size=(img_size, img_size),
+        validation_split=0.8,
+        seed=2**20,
+        subset="both",
+        interpolation="bilinear",
+        crop_to_aspect_ratio=False,
+    )
     if small_test:
-        training = training.take(400)
-        validation = validation.take(100)
-    return training, validation
+        return training.take(400), validation.take(100)
+    else:
+        return training, validation
